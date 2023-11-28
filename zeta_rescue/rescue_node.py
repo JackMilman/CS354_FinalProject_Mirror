0bed9af3d9e86e9e030800249c853aa01b063d4a
Jack Milman <jpm1228@gmail.com>
2023-11-28 17:25:44 -0500

    navigation mostly moved out of main, no more thrashing with spinning up and down. Work still required to get the thing to go back to the start.

--- zeta_rescue/rescue_node.py
+++ zeta_rescue/rescue_node.py
@@ -22,12 +22,19 @@ import rclpy.node
 from rclpy.action.client import ActionClient
 from rclpy.task import Future
 
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray
 
 import tf_transformations
 from zeta_competition_interfaces.msg import Victim
 
 from ros2_aruco_interfaces.msg import ArucoMarkers
+
+def make_random_goal():
+    x = random.uniform (-2.0, 2.0)
+    y = random.uniform (-2.0, 2.0)
+    theta = random.uniform (-np.pi, np.pi)
+    random_goal = create_nav_goal(x, y, theta)
+    return random_goal
 
 def create_nav_goal(x, y, theta):
     goal = NavigateToPose.Goal()
@@ -56,7 +63,7 @@ def create_nav_goal_quat(x, y, quaternion):
 
 class RescueNode(rclpy.node.Node):
 
-    def __init__(self, goal, timeout):
+    def __init__(self, timeout, iterations):
         super().__init__('rescue_node')
 
         # This QOS Setting is used for topics where the messages
@@ -66,28 +73,48 @@ class RescueNode(rclpy.node.Node):
         latching_qos = QoSProfile(depth=1,
                                   durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
 
-        self.create_subscription(OccupancyGrid, 'map',
-                                 self.map_callback,
-                                 qos_profile=latching_qos)
-        self.create_subscription(PoseWithCovarianceStamped, 'amcl_pose', self.test_callback, 10)
+        # self.create_subscription(OccupancyGrid, 'map',
+        #                          self.map_callback,
+        #                          qos_profile=latching_qos)
+        self.create_subscription(PoseWithCovarianceStamped, 'amcl_pose', self.init_pose_callback, 10)
         # self.create_publisher(Victim, 'victim_pose', self.timer_callback, 10)
-        self.create_subscription(ArucoMarkers, 'aruco_markers', self.scanner_callback, 10)
-        self.goal = goal
+        # self.create_subscription(PoseArray, 'aruco_poses', self.scanner_callback, 10)
+
+        self.wandering = False
+
+        # self.goal = goal
+        self.goal = None
+        self.goal_future = Future()
         self.initial_pose = None
 
         # Create the action client.
         self.ac = ActionClient(self, NavigateToPose, '/navigate_to_pose')
+        self.completed_navs = 0
+        self.navigation_complete = False
+        self.max_iterations = iterations
+        self.create_timer(.1, self.timer_callback)
+        self.create_timer(1, self.wander_callback)
 
         # Used if we decide to cancel a goal request. None indicates
         # we haven't canceled.
         self.cancel_future = None
+        # Used to keep track of the entire node's progress and to exit when we want to.
+        self.node_future = Future()
         
-        self.map = None
+        # self.map = None
         self.timeout = timeout
+
+    def get_future(self):
+        return self.node_future
+    
+    def start_wandering(self):
+        if not self.wandering:
+            first_goal = make_random_goal()
+            self.update_goal(first_goal)
     
     def update_goal(self, new_goal):
         self.goal = new_goal
-        self.ac = ActionClient(self, NavigateToPose, '/navigate_to_pose')
+        self.send_goal()
     
     def send_goal(self):
         self.get_logger().info("WAITING FOR NAVIGATION SERVER...")
@@ -97,73 +124,43 @@ class RescueNode(rclpy.node.Node):
         self.start_time = time.time()
 
         self.goal_future = self.ac.send_goal_async(self.goal)
-
-        self.create_timer(.1, self.timer_callback)
-
-        # This will be used by rclpy to know when this node has
-        # finished its work.
-        self.future_event = Future()
-
-        return self.future_event
     
     def scanner_callback(self, aruco_msg):
         self.get_logger().info("Did you find something?")
-        for marker in aruco_msg.markers:
-            if marker.id == 0:
-                self.get_logger().info("Found a victim!")
-                victim = Victim()
-                victim.header.frame_id = "map"
-                victim.pose.pose.position.x = marker.pose.position.x
-                victim.pose.pose.position.y = marker.pose.position.y
-                victim.pose.pose.position.z = marker.pose.position.z
-                victim.pose.pose.orientation.x = marker.pose.orientation.x
-                victim.pose.pose.orientation.y = marker.pose.orientation.y
-                victim.pose.pose.orientation.z = marker.pose.orientation.z
-                victim.pose.pose.orientation.w = marker.pose.orientation.w
-                victim.pose.covariance = marker.pose.covariance
-                victim.id = marker.id
-                victim.distance = marker.distance
-                victim.confidence = marker.confidence
-                self.victim_pub.publish(victim)
-                self.get_logger().info("Published victim!")
-            else:
-                self.get_logger().info("Found a marker, but not a victim.")
+        self.get_logger().info("Poses:")
+        for pose in aruco_msg.poses:
+            self.get_logger().info(f"X = {pose.position.x:.2f}")
+            self.get_logger().info(f"Y = {pose.position.y:.2f}")
 
-                
-        
-    def map_callback(self, map_msg):
-        """Process the map message.
-
-        This doesn't really do anything useful, it is purely intended
-        as an illustration of the Map class.
-
-        """
-        if self.map is None:  # No need to do this every time map is published.
-
-            self.map = map_utils.Map(map_msg)
-
-            # Use numpy to calculate some statistics about the map:
-            total_cells = self.map.width * self.map.height
-            pct_occupied = np.count_nonzero(self.map.grid == 100) / total_cells * 100
-            pct_unknown = np.count_nonzero(self.map.grid == -1) / total_cells * 100
-            pct_free = np.count_nonzero(self.map.grid == 0) / total_cells * 100
-            map_str = "Map Statistics: occupied: {:.1f}% free: {:.1f}% unknown: {:.1f}%"
-            self.get_logger().info(map_str.format(pct_occupied, pct_free, pct_unknown))
-
-            # Here is how to access map cells to see if they are free:
-            x = self.goal.pose.pose.position.x
-            y = self.goal.pose.pose.position.y
-            val = self.map.get_cell(x, y)
-            if val == 100:
-                free = "occupied"
-            elif val == 0:
-                free = "free"
-            else:
-                free = "unknown"
-            self.get_logger().info(f"HEY! Map position ({x:.2f}, {y:.2f}) is {free}")
+    """
+    This method is keeping track of the overall status of the node's search
+    """
+    def wander_callback(self):
+        if self.node_future.done():
+            self.get_logger().info("RESCUEBOT RETURNING HOME!")
+        elif self.goal_future.done():
+            if self.completed_navs >= self.max_iterations:
+                self.get_logger().info("MAXIMUM ITERATIONS REACHED, EXITING!")
+                self.node_future.set_result(True)
+            elif self.navigation_complete:
+                self.navigation_complete = False
+                self.completed_navs += 1
+                self.get_logger().info(f"{self.completed_navs}")
+                self.get_logger().info("RESCUEBOT CONTINUING SEARCH IN NEW POSITION")
+                x = random.uniform (-2.0, 2.0)
+                y = random.uniform (-2.0, 2.0)
+                theta = random.uniform (-np.pi, np.pi)
+                new_goal = create_nav_goal(x, y, theta)
+                self.update_goal(new_goal)
+        elif not self.wandering:
+            self.start_wandering()
 
     def timer_callback(self):
         """Periodically check in on the progress of navigation."""
+
+        if self.completed_navs >= self.max_iterations:
+            self.get_logger().info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            self.cancel_future = self.goal_future.result().cancel_goal_async()
 
         if not self.goal_future.done():
             self.get_logger().info("NAVIGATION GOAL NOT YET ACCEPTED")
@@ -171,59 +168,54 @@ class RescueNode(rclpy.node.Node):
         elif self.cancel_future is not None:  # We've cancelled and are waiting for ack.
             if self.cancel_future.done():
                 self.get_logger().info("SERVER HAS ACKNOWLEDGED CANCELLATION")
-                self.ac.destroy()
-                self.future_event.set_result(False)
+                # self.ac.destroy()
+                self.node_future.set_result(False)
+                # self.future_event.set_result(False)
         else:
+            if not self.navigation_complete:
+                if self.goal_future.result().status == GoalStatus.STATUS_SUCCEEDED:
+                    self.get_logger().info("NAVIGATION SERVER REPORTS SUCCESS!")
+                    self.navigation_complete = True
+                    # self.ac.destroy()
+                    # self.future_event.set_result(True)
+                elif self.goal_future.result().status == GoalStatus.STATUS_ABORTED:
+                    self.get_logger().info("NAVIGATION SERVER HAS ABORTED.")
+                    self.node_future.set_result(False)
+                    self.navigation_complete = True
+                    # self.ac.destroy()
+                    # self.future_event.set_result(False)
 
-            if self.goal_future.result().status == GoalStatus.STATUS_SUCCEEDED:
-                self.get_logger().info("NAVIGATION SERVER REPORTS SUCCESS. EXITING!")
-                self.ac.destroy()
-                self.future_event.set_result(True)
-
-            if self.goal_future.result().status == GoalStatus.STATUS_ABORTED:
-                self.get_logger().info("NAVIGATION SERVER HAS ABORTED. EXITING!")
-                self.ac.destroy()
-                self.future_event.set_result(False)
-
-            elif time.time() - self.start_time > self.timeout:
-                self.get_logger().info("TAKING TOO LONG. CANCELLING GOAL!")
-                self.cancel_future = self.goal_future.result().cancel_goal_async()
+            # elif time.time() - self.start_time > self.timeout:
+            #     self.get_logger().info("TAKING TOO LONG. CANCELLING GOAL!")
+            #     self.cancel_future = self.goal_future.result().cancel_goal_async()
+        
     
-    def test_callback(self, amcl_pose):
+    def init_pose_callback(self, amcl_pose):
         if self.initial_pose is None:
             self.initial_pose = amcl_pose
-        self.get_logger().info("I AM ALIVE")
+            self.get_logger().info("Initial pose set")
 
 
 def main():
     rclpy.init()
-    x = random.uniform (-2.0, 2.0)
-    y = random.uniform (-2.0, 2.0)
-    theta = random.uniform (-np.pi, np.pi)
+
     timeout = float('inf')
-    first_goal = create_nav_goal(x, y, theta)
-    node = RescueNode(first_goal, timeout)
-    # Arbitrary wandering number for testing purposes, can change later.
-    i = 0
-    while i < 2:
-        x = random.uniform (-2.0, 2.0)
-        y = random.uniform (-2.0, 2.0)
-        theta = random.uniform (-np.pi, np.pi)
-        new_goal = create_nav_goal(x, y, theta)
-        node.update_goal(new_goal)
-        future = node.send_goal()
-        rclpy.spin_until_future_complete(node, future)
-        node.get_logger().info("Node's future: " + str(future.result()))
-        i += 1
+    # test parameter for determining how long to randomly wander, change later
+    iterations = 2
+    node = RescueNode(timeout, iterations)
+    node_future = node.get_future()
+    rclpy.spin_until_future_complete(node, node_future)
+
     # Navigates back to the initial position and then exits cleanly we hope.
     initial_x = node.initial_pose.pose.pose.position.x
     initial_y = node.initial_pose.pose.pose.position.y
     initial_orient = node.initial_pose.pose.pose.orientation
     go_back = create_nav_goal_quat(initial_x, initial_y, initial_orient)
     node.update_goal(go_back)
-    future = node.send_goal()
-    rclpy.spin_until_future_complete(node, future)
-    node.get_logger().info("Node's future: " + str(future.result()))
+    node.node_future = Future()
+    node_future = node.get_future()
+    rclpy.spin_until_future_complete(node, node_future)
+    node.get_logger().info("Node's future: " + str(node_future.result()))
 
 
 
